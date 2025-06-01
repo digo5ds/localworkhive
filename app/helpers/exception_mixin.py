52fc770eb983764307c1a6685ad675a3a62dacca
digo5ds <diogo.silva5ds@outlook.com>
2025-06-01 17:52:25 -0300

    fixing botocore mixin exception

--- app/helpers/exception_mixin.py
+++ app/helpers/exception_mixin.py
@@ -1,7 +1,5 @@
-"""
-This module provides utilities for handling
-common exceptions when interacting with AWS services using boto3/botocore.
-"""
+"""This module provides utilities for handling common exceptions when
+interacting with AWS services using boto3/botocore."""
 
 from functools import wraps
 
@@ -14,16 +12,12 @@ from botocore.exceptions import (
 
 
 class BotoExceptionHandlingMixin:
-    """
-    A mixin class that provides a method to safely execute
-    functions interacting with AWS services,
-    handling common boto3 exceptions.
-    """
+    """A mixin class that provides a method to safely execute functions
+    interacting with AWS services, handling common boto3 exceptions."""
 
     def safe_execute(self, func, *args, **kwargs):
-        """
-        Executes a function safely by catching and handling specific
-        AWS-related exceptions.
+        """Executes a function safely by catching and handling specific AWS-
+        related exceptions.
 
         This method attempts to execute the provided
         function with the given arguments and keyword arguments.
@@ -50,27 +44,36 @@ class BotoExceptionHandlingMixin:
             return func(*args, **kwargs)
         except (
             ClientError,
-            EndpointConnectionError,
             NoCredentialsError,
             PartialCredentialsError,
         ) as e:
-            print(f"[ERROR]: {type(e).__name__} - {str(e)}")
-            raise e
+            raise ValueError(
+                f"An error occurred while executing the function: {str(e)}"
+            ) from e
+        except EndpointConnectionError as e:
+            raise RuntimeError(
+                f"[ERROR]: Endpoint connection error, if running with localstack verify if localstack is running: {e}"
+            ) from e
 
 
 def exception_safe(method):
-    """
-    Decorator to wrap a method in safe_execute to catch and handle exceptions.
+    """Decorator to wrap a method in safe_execute to catch and handle
+    exceptions.
 
-    The wrapper calls the wrapped method with the provided arguments and keyword arguments,
-    and catches any exceptions of type ClientError, EndpointConnectionError, NoCredentialsError,
-    and PartialCredentialsError. If an exception is caught, it is printed to the console and
-    re-raised.
+    The wrapper calls the wrapped method with the provided arguments and
+    keyword arguments, and catches any exceptions of type ClientError,
+    EndpointConnectionError, NoCredentialsError, and
+    PartialCredentialsError. If an exception is caught, it is printed to
+    the console and re-raised.
 
-    :param self: The instance of the class containing the wrapped method.
-    :param args: The positional arguments to be passed to the wrapped method.
-    :param kwargs: The keyword arguments to be passed to the wrapped method.
-    :return: The result of calling the wrapped method, or None if an exception was caught.
+    :param self: The instance of the class containing the wrapped
+        method.
+    :param args: The positional arguments to be passed to the wrapped
+        method.
+    :param kwargs: The keyword arguments to be passed to the wrapped
+        method.
+    :return: The result of calling the wrapped method, or None if an
+        exception was caught.
     """
 
     @wraps(method)
