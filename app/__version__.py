03ccb10cc944fb17991cfa0c4d11be656ea7348e
digo5ds <diogo.silva5ds@outlook.com>
2025-06-01 17:50:51 -0300

    creating file version

---
+++ app/__version__.py
@@ -0,0 +1,8 @@
+"version.py - Version management for the application."
+
+__version__ = "1.0.0"  # Update this version as needed
+
+
+def get_version():
+    """Returns the current version of the application."""
+    return __version__
