957b05ee630f6c460e6f74d8bde73782c9b060bf
digo5ds <diogo.silva5ds@outlook.com>
2025-06-07 23:07:00 -0300

    fixing interfaces

--- app/interfaces/aws_resources_interface.py
+++ app/interfaces/aws_resources_interface.py
@@ -94,3 +94,69 @@ class ResourcesInterface(ABC, BotoExceptionHandlingMixin):
         botocore.exceptions.ClientError: If the operation fails.
         """
         raise NotImplementedError()
+
+
+class SqsInterface(ABC, BotoExceptionHandlingMixin):
+    """Base interface for managing AWS resources.
+
+    This class defines abstract methods that must be implemented
+    by any resource class interacting with AWS services. It enforces
+    a consistent interface for resource creation, retrieval, and deletion,
+    along with standardized exception handling through BotoExceptionHandlingMixin.
+
+    Inherits:
+        ABC: For defining abstract base classes.
+        BotoExceptionHandlingMixin: For handling Boto3-related exceptions.
+
+    Abstract methods:
+        new_resource: Create a new AWS resource.
+        delete_resource: Delete a specific AWS resource.
+        get_resources: Retrieve resources.
+        delete_resources: Delete AWS resources.
+    """
+
+    @abstractmethod
+    def send_message(self, resource_model):
+        """Creates a new AWS SQS resource.
+
+        Parameters:
+        resource_model: The model of the resource to be created.
+
+        Returns:
+        str: The URL of the created SQS queue.
+
+        Raises:
+        ValueError: If the resource already exists.
+        botocore.exceptions.ClientError: If the operation fails.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def receive_message(self, resource_model):
+        """Receives messages from an AWS SQS queue.
+
+        Parameters:
+        resource_model: The model of the resource to be received.
+
+        Returns:
+        dict: The received messages.
+
+        Raises:
+        botocore.exceptions.ClientError: If the operation fails.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def delete_message(self, resource_model):
+        """Deletes a message from an AWS SQS queue.
+
+        Parameters:
+        resource_model: The model of the resource to be deleted.
+
+        Returns:
+        bool: True if the message was deleted.
+
+        Raises:
+        botocore.exceptions.ClientError: If the operation fails.
+        """
+        raise NotImplementedError()
